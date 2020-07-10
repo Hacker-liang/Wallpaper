1d08460da92e608c144c82e1aa7dd6dcdbe627f9
langren <pengshuai.liang@gmail.com>
2020-07-10 20:29:43 +0800

    添加七牛云sdk

--- Podfile
+++ Podfile
@@ -2,4 +2,5 @@
 target 'Wallpaper' do
     pod 'Alamofire'
     pod 'SnapKit'
+    pod "Qiniu", "~> 7.4.1"
 end
--- Wallpaper/ViewController.swift
+++ Wallpaper/ViewController.swift
@@ -20,16 +20,16 @@ class ViewController: UIViewController {
         livePhotoView = PHLivePhotoView(frame: self.view.bounds)
         self.view.addSubview(livePhotoView)
         
-        let tap = UITapGestureRecognizer()
-        tap.numberOfTouchesRequired = 1
-        tap.numberOfTapsRequired = 1
-        tap.addTarget(self, action: #selector(livePhotoViewPress))
-        livePhotoView.addGestureRecognizer(tap)
+//        let tap = UITapGestureRecognizer()
+//        tap.numberOfTouchesRequired = 1
+//        tap.numberOfTapsRequired = 1
+//        tap.addTarget(self, action: #selector(livePhotoViewPress))
+//        livePhotoView.addGestureRecognizer(tap)
         
-        guard let jpgFilePath = Bundle.main.path(forResource: "asnw", ofType: "jpg"), let jpgUrl = URL(string: jpgFilePath), let movFilePath = Bundle.main.path(forResource: "asnw", ofType: "mov"), let movUrl = URL(string: movFilePath) else {
+        guard let jpgFilePath = Bundle.main.path(forResource: "apple", ofType: "jpg"), let jpgUrl = URL(string: jpgFilePath), let movFilePath = Bundle.main.path(forResource: "apple", ofType: "mov"), let movUrl = URL(string: movFilePath) else {
             return
         }
-        PHLivePhoto.request(withResourceFileURLs: [jpgUrl, movUrl], placeholderImage: nil, targetSize: CGSize(width: 375, height: 667), contentMode: .aspectFill) { (photo, result) in
+        PHLivePhoto.request(withResourceFileURLs: [jpgUrl,movUrl], placeholderImage: UIImage(named: "asnw.jpg"), targetSize: CGSize(width: 375, height: 667), contentMode: .aspectFill) { (photo, result) in
             if let p = photo {
                 self.updateLivePhoto(livePhoto: p)
             }
